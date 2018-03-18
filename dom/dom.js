1a0015b22229f80bd8bd33b5cacce7b881555b74
Paul Boeck <paul.boeck@cgi.com>
2018-03-18 22:00:24 +0100

    Day 11 - Add/Remove

--- dom/dom.js
+++ dom/dom.js
@@ -4,7 +4,7 @@ var btnColor = document.querySelector('#btnChangeColor');
 console.log(btnColor);
 
 function rand(n) {
-    return Math.floor(Math.random()*(n +1));
+    return Math.floor(Math.random() * (n + 1));
 }
 
 function changeBodyColor() {
@@ -27,17 +27,41 @@ var btnSearch = document.querySelector('#btnSearch');
 
 var contacts = ['Chris', 'Sarah', 'Bill', 'Mary'];
 
-btnSearch.onclick = function() {
+btnSearch.onclick = function () {
     var searchName = inputSearch.value;
     console.log(searchName);
- 
+
     var position = contacts.indexOf(searchName);
     console.log(position);
     if (position >= 0) {
-        outputSearch.textContent = searchName + 
+        outputSearch.textContent = searchName +
             '\'s position is ' + position;
     } else {
         outputSearch.textContent = 'Could not find ' +
             searchName;
     }
- } 
+}
+
+/****** Adding to the DOM *****/
+function buildNewColorElement(color) {
+    var colorElement = document.createElement('li');
+    colorElement.textContent = color;
+    colorElement.style.backgroundColor = color;
+
+    var btnRemove = document.createElement('button');
+    btnRemove.textContent = 'Remove';
+    btnRemove.onclick = () => colorElement.remove();
+    colorElement.appendChild(btnRemove);
+    return colorElement;
+}
+
+function addColor(color) {
+    var newColor = document
+        .querySelector('#txtNewColor')
+        .value;
+
+    var colorList = document.querySelector("#colorList");
+    // in addColor
+    var newEl = buildNewColorElement(newColor);
+    colorList.appendChild(newEl);
+}
